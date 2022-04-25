fb9f0e0ce5196a2c5e6cc0f52e6cc18d9c39ca0d
JamesChavess <you@example.com>
2022-04-25 13:52:40 -0500

    real time data base connected and functioning

--- upgrade-english-app/src/firebase.js
+++ upgrade-english-app/src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getAuth} from 'firebase/auth'
+import { getAuth } from "firebase/auth";
+import { DataSnapshot, getDatabase, onValue, ref, set } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDpKbZo3RQsxHyfJ7ibRNF9QVk9dzTlvXU",
@@ -9,40 +10,60 @@ const firebaseConfig = {
   storageBucket: "english-stats-2022.appspot.com",
   messagingSenderId: "1071451304718",
   appId: "1:1071451304718:web:eeded646c931d006889d5f",
-  measurementId: "G-NB9X50HLB4"
+  measurementId: "G-NB9X50HLB4",
 };
 
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
-export const auth = getAuth(app)
+export const auth = getAuth(app);
 
+const db = getDatabase();;
+let userId = 57
+// setter functionality below
 
+function writeUserData(userId, name, email, level, scores, imgUrl) {
+  const db = getDatabase();
+  const reference = ref(db, "users/" + userId);
+  set(reference, {
+    userName: name,
+    email: email,
+    level: level,
+    scores: scores,
+    profilePic: imgUrl,
+  });
+  console.log("write data function executed");
+}
 
+//getter functionality below 
 
+const scores = ref(db, 'users/'+ userId +'/scores' )
+console.log(scores)
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+onValue(scores,(snapshot)=>{
+  const data = snapshot.val();
+  console.log(data)
+})
 
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import {getAuth} from 'firebase/auth'
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC81jrn8COanxj0jrH3TWTYnhyU3YBQkQU",
-//   authDomain: "english-stats-app.firebaseapp.com",
-//   projectId: "english-stats-app",
-//   storageBucket: "english-stats-app.appspot.com",
-//   messagingSenderId: "944483565304",
-//   appId: "1:944483565304:web:bdedb8cce0118f077d850d",
-//   measurementId: "G-C61XRXX4E9"
-// };
-
-// // Initialize Firebase
-// export const app = initializeApp(firebaseConfig);
-// export const analytics = getAnalytics(app);
-// export const auth = getAuth(app)
+writeUserData(
+  57,
+  "james A chaves",
+  "757575757@gmail.com",
+  "advanced",
+  {
+    2020: {
+      comprehension: 60,
+      fluency: 20,
+      sentenceStructure: 40,
+      pronunciation: 80,
+      monoLing: 30,
+    },
+    2021: {
+      comprehension: 55,
+      fluency: 30,
+      sentenceStructure: 50,
+      pronunciation: 85,
+      monoLing: 50,
+    },
+  },
+  "../src/imgs/profilePic.png"
+);
